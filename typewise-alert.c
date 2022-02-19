8df381792fd3814c75239d5db68660fb4d263cad
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-02-20 00:20:10 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -1,6 +1,12 @@
 #include "typewise-alert.h"
 #include <stdio.h>
 
+Limits parameterLimits[MAX_COOLING] = {
+  {0,35},
+  {0,45},
+  {0,40}
+};
+
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
   if(value < lowerLimit) {
     return TOO_LOW;
@@ -12,23 +18,23 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
 }
 
 BreachType classifyTemperatureBreach(
-    CoolingType coolingType, double temperatureInC) {
-  int lowerLimit = 0;
-  int upperLimit = 0;
-  switch(coolingType) {
-    case PASSIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 35;
-      break;
-    case HI_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 45;
-      break;
-    case MED_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 40;
-      break;
-  }
+    CoolingType coolingType, double temperatureInC, Limits parameterLimits[]) {
+  int lowerLimit = parameterLimits[coolingType].lowerLimit;
+  int upperLimit = parameterLimits[coolingType].higherLimit;
+//   switch(coolingType) {
+//     case PASSIVE_COOLING:
+//       lowerLimit = 0;
+//       upperLimit = 35;
+//       break;
+//     case HI_ACTIVE_COOLING:
+//       lowerLimit = 0;
+//       upperLimit = 45;
+//       break;
+//     case MED_ACTIVE_COOLING:
+//       lowerLimit = 0;
+//       upperLimit = 40;
+//       break;
+//   }
   return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
 
@@ -36,7 +42,7 @@ void checkAndAlert(
     AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
 
   BreachType breachType = classifyTemperatureBreach(
-    batteryChar.coolingType, temperatureInC
+    batteryChar.coolingType, temperatureInC, parameterLimits
   );
 
   switch(alertTarget) {
